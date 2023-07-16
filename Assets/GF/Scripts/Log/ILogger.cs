05dfed0711700fe600a9abd9e489582ebd32f22a
lishun001 <shun.li@castbox.fm>
2023-07-17 00:53:08 +0800

    官方sample

--- Assets/GF/Scripts/Log/ILogger.cs
+++
@@ -1,9 +0,0 @@
-namespace GF
-{
-    public interface ILogger
-    {
-        void I(string msg, params object[] args);
-        void W(string msg, params object[] args);
-        void E(string msg, params object[] args);
-    }
-}
